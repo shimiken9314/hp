441611099706a3c6b5baad844a063313e0ecf453
shimiken9314 <shimiken9314@shimiken9314.monster>
2025-04-10 16:11:33 +0900

    Add direct profile.html for simpler access

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// シミケンプロフィールページ（HTMLアクセス用）
+Route::get('/profile.html', function () {
+    return view('pages.profile');
+});
